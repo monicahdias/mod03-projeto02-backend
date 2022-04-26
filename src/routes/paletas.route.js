94d4b46bc90482268deb4c174e77004b4703d392
Monica Sumie Hieda Dias <msumiedias@gmail.com>
2022-04-26 20:49:55 -0300

    swagger

--- src/routes/paletas.route.js
+++ src/routes/paletas.route.js
@@ -1,12 +1,17 @@
 const route = require('express').Router();
 const controllerPaletas = require('../controllers/paletas.controller');
 const controllerCarrinho = require('../controllers/carrinho.controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
 const {
   validId,
   validObjectBody,
   validObjectBodyCarrinho,
 } = require('../middlewares/paleta.middleware');
+
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 route.get('/all-paletas', controllerPaletas.findAllPaletasController);
 route.get(
